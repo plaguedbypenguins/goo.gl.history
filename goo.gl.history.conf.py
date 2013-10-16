d409f5dc4b57e917e56bf265d0ca1e4cc44abad7
Robin Humble <plaguedbypenguins@gmail.com>
2013-10-16 15:45:30 +1100

    add an example config file

---
+++ goo.gl.history.conf.py
@@ -0,0 +1,10 @@
+
+# dict of file group: and their [ goo.gl urls ]
+groups = { 'b2g':['YYgisb', '7rr5ES'],
+          'cm10':['2s2fEm', 'ReLprf'] }
+
+# db of previous goo.gl lookups
+dbName = 'goo.gl.pickle'
+
+# 2 letter country codes for some countries
+countryName = 'country.txt'
